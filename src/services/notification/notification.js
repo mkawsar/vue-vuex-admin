4ac084861db4fec6376ad524d246d94aced95bc4
mkawsar <kawsar.diu888@gmail.com>
2022-03-01 13:34:23 +0600

    implemented multiple notification with error message

--- src/services/notification/notification.js
+++ src/services/notification/notification.js
@@ -4,6 +4,46 @@ export default {
         this.notifyTemplate($this, 'top', 'right', template, 'ti-info-alt', 'success');
     },
 
+    error($this, title, text) {
+        let template = '<span><b>' + title + '</b> - ' + text + '</span>';
+        this.notifyText($this, 'top', 'right', template, 'ti-info-alt', 'danger');
+    },
+
+    notifyValidationErrors($this, notifications) {
+        if (typeof notifications === 'string') {
+            let template = '<span><b>' + title + '</b> - ' + text + '</span>';
+            this.notifyText($this, 'top', 'right', template, 'ti-info-alt', 'danger');
+        } else if (notifications.constructor === Array) {
+            let template = '<ul style="list-style-type: disc">';
+            for (let key in notifications) {
+                if (notifications.hasOwnProperty(key)) {
+                    template += '<li>' + notifications[key] + '</li>';
+                }
+            }
+            template += '</ul>';
+            this.notifyText($this, 'top', 'right', template, 'ti-info-alt', 'danger');
+        }
+
+    },
+
+    notifyError($this, notifications) {
+        if (notifications.hasOwnProperty('message')) {
+            if (typeof notifications.message === 'string') {
+                let template = '<span><b>Oops!</b> - ' + notifications.message + '</span>';
+                this.notifyText($this, 'top', 'right', template, 'ti-info-alt', 'danger');
+            } else if (typeof notifications.message === 'object') {
+                let template = '<ul>';
+                for (let key in notifications.message) {
+                    if (notifications.message.hasOwnProperty(key)) {
+                        template += '<li>' + notifications.message[key][0] + '</li>';
+                    }
+                }
+                template += '</ul>';
+                this.notifyText($this, 'top', 'right', template, 'ti-info-alt', 'danger');
+            }
+        }
+    },
+
     notifyTemplate($this, verticalAlign, horizontalAlign, template, icon, type) {
         $this.$notify({
             component: {
