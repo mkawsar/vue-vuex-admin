beac593142c9ccc26c33b6c164250343fc3a113c
mkawsar <kawsar.diu888@gmail.com>
2022-03-08 16:46:38 +0600

    update config

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 export default {
-    HOST_URL: 'http://127.0.0.1:8000/api/v1',
+    HOST_URL: 'http://127.0.0.1:8000/',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
